2affd4ebee34615b0bd486da1606d75d7f1dfe3d
Rafael Dalmarco <111667173+rdalmarco@users.noreply.github.com>
2023-10-01 14:50:57 -0300

    01102023-1431 : Cada usuário agora tem seu arquivo de user + pass e key + iv, isso permite que sejam cadastrados vários usuários no sistema, e também que os arquivos de key + iv sejam criptografados utilizando AES_ECB. Testado e a princípio finalizado

--- src/main/java/org/exercicio_seg_1/dao/usersRepository.java
+++ src/main/java/org/exercicio_seg_1/dao/usersRepository.java
@@ -68,10 +68,11 @@ public class usersRepository {
 
                 readerIvAndKey.close();
 
+                File readerIvAndKey_temp = new File("KeyAndIv_" + usernameCriptografado + "_temp.txt");
+                readerIvAndKey_temp.delete();
+
                 if (aesCbc.decrypter(senhaCriptografada, chaveSecreta, iv).equals(password)) {
                     System.out.println("Usuario logado");
-                    File readerIvAndKey_temp = new File("KeyAndIv_" + usernameCriptografado + "_temp.txt");
-                    readerIvAndKey_temp.delete();
                     return true;
                 } else {
                     System.out.println("Senha incorreta");
